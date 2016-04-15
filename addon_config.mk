b26b2ce1b22901b74d5cedb9484ad6f4a26c481c
Christopher Baker <me@christopherbaker.net>
2016-04-15 09:25:52 -0700

    Update addon_config.mk

--- addon_config.mk
+++ addon_config.mk
@@ -2,5 +2,5 @@ meta:
 	ADDON_NAME = ofxSpatialHash
 	ADDON_DESCRIPTION = A collection of 2D/3D spatial hashing algorithms.
 	ADDON_AUTHOR = bakercp
-	ADDON_TAGS = "nanoflann" "flann" "nearest neighbor" "spatial hash" "point cloud" "particles"
+	ADDON_TAGS = "kdtree" "nanoflann" "flann" "nearest neighbor" "spatial hash" "point cloud" "particles"
 	ADDON_URL = http://github.com/bakercp/ofxSpatialHash
